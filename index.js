e6a0e29565ddf00e19f03e0445ad16ef57e3822a
Sumin <csumin000825@gmail.com>
2021-11-08 20:59:50 +0900

    옵셔널 파라미터

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello codeit!')
+//옵셔널 파라미터
+
+function introduce(name, age = 15){
+    console.log(`${name}님, 당신의 나이는 ${age}살입니다.`);
+}
+
+introduce('수민', 22);
+introduce('재현');
+introduce();
