9e5306063dfdc54d78ddd96b58b36aeeabce1320
Sumin <csumin000825@gmail.com>
2021-11-08 21:25:59 +0900

    실습. 세트메뉴 주문하기

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-//옵셔널 파라미터
+// 10. 실습: 세트메뉴 주문하기
 
-function introduce(name, age = 15){
-    console.log(`${name}님, 당신의 나이는 ${age}살입니다.`);
+// setMenuOrder라는 이름의 함수를 만드세요.
+// 여기에 코드를 작성해주세요.
+sandwich = prompt("샌드위치 이름을 입력해주세요.");
+drink = prompt("음료 이름을 입력해주세요.");
+
+function orderSetMenu(sandwich, drink){
+    console.log(`주문하신 ${sandwich}, ${drink} 세트메뉴 나왔습니다!`);
 }
 
-introduce('수민', 22);
-introduce('재현');
-introduce();
+// 테스트 코드
+orderSetMenu(sandwich, drink);
