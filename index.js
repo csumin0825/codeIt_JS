11840316f2065ef6c79d16d96497d681de78cc91
Sumin <csumin000825@gmail.com>
2021-11-12 15:38:53 +0900

    십습. 학점 계산기

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-// 파라미터 height을 활용하는 checkHeight 함수를 완성하세요.
-function checkHeight(height){
-    if(height >= 140){
-        console.log("탑승이 가능합니다.");
-    }else{
-        console.log("탑승이 불가능합니다.");
-    }
+// 실습: 학점 계산기
+
+function printGrade(midtermScore, finalScore){
+	let totalScore = midtermScore + finalScore;
+  // 여기에 코드를 작성해 주세요.
+  if(totalScore >= 90){
+      console.log('A');
+  }else if(totalScore >= 80){
+      console.log('B');
+  }else if(totalScore>=70){
+      console.log('C');
+  }else if(totalScore >= 60){
+      console.log('D');
+  }else{
+      console.log('F');
+  }
 }
 
 // 테스트 코드
-checkHeight(141);
-checkHeight(135);
-checkHeight(170);
+printGrade(25, 35);
+printGrade(50, 45);
+printGrade(29, 24);
+printGrade(37, 42);
