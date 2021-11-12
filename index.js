8efb91584284c9160134c6d87cf428ca17498f3b
Sumin <csumin000825@gmail.com>
2021-11-12 14:58:35 +0900

    실습. 함수 변수 복습하기

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-// 상수(constant)
-const PI = 3.14;  // 상수는 대문자
-let radius = 0;
+// 실습 : 함수 변수 복습하기
 
-function area(){
-    return `반지름의 길이 : ${radius}, 원의 넓이 : ${radius*PI}`;
+// adultTag, teenagerTag, errorTag, transferTag라는 이름의 변수들을 만드세요.
+// 여기에 코드를 작성해 주세요.
+let adultTag = "삑!";
+let teenagerTag = "삑삑!";
+let errorTag = "삑삑삑!";
+let transferTag = "환승입니다.";
+
+// tagCase파라미터를 가지는 tagNotification라는 이름의 함수를 만드세요.
+// 여기에 코드를 작성해 주세요.
+function tagNotification(tagCase){
+    console.log(tagCase);
 }
 
-radius = 5;
-console.log(area());
+
+// 테스트 코드
+tagNotification(adultTag);
+tagNotification(teenagerTag);
+tagNotification(transferTag);
+tagNotification(errorTag);
+tagNotification(adultTag);
