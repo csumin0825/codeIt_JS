cb0b591a7ab4c8b0801fe779f52688614edb3bff
Sumin <csumin000825@gmail.com>
2021-11-12 14:48:28 +0900

    Scope: 범위, 영역

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-// 10. 실습: 세트메뉴 주문하기
+// Scope : 범위, 영역
+let x = 3;
 
-// setMenuOrder라는 이름의 함수를 만드세요.
-// 여기에 코드를 작성해주세요.
-sandwich = prompt("샌드위치 이름을 입력해주세요.");
-drink = prompt("음료 이름을 입력해주세요.");
-
-function orderSetMenu(sandwich, drink){
-    console.log(`주문하신 ${sandwich}, ${drink} 세트메뉴 나왔습니다!`);
+function myFunction(){  // 블록문
+    let x = 5;
+    console.log(x);
 }
 
-// 테스트 코드
-orderSetMenu(sandwich, drink);
+myFunction();
+console.log(x);
