5c4f1dfc3307e3e91ae37186b0629e14df6c5375
Sumin <csumin000825@gmail.com>
2021-11-12 14:52:30 +0900

    상수

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// Scope : 범위, 영역
-let x = 3;
+// 상수(constant)
+const PI = 3.14;  // 상수는 대문자
+let radius = 0;
 
-function myFunction(){  // 블록문
-    let x = 5;
-    console.log(x);
+function area(){
+    return `반지름의 길이 : ${radius}, 원의 넓이 : ${radius*PI}`;
 }
 
-myFunction();
-console.log(x);
+radius = 5;
+console.log(area());
