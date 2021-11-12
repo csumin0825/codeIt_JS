ba194a4dca4fa4ba4ef18162628b956864315202
Sumin <csumin000825@gmail.com>
2021-11-12 15:20:13 +0900

    실습. 롤러코스터, 탈 수 있을까?

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-// 실습 : 함수 변수 복습하기
-
-// adultTag, teenagerTag, errorTag, transferTag라는 이름의 변수들을 만드세요.
-// 여기에 코드를 작성해 주세요.
-let adultTag = "삑!";
-let teenagerTag = "삑삑!";
-let errorTag = "삑삑삑!";
-let transferTag = "환승입니다.";
-
-// tagCase파라미터를 가지는 tagNotification라는 이름의 함수를 만드세요.
-// 여기에 코드를 작성해 주세요.
-function tagNotification(tagCase){
-    console.log(tagCase);
+// 파라미터 height을 활용하는 checkHeight 함수를 완성하세요.
+function checkHeight(height){
+    if(height >= 140){
+        console.log("탑승이 가능합니다.");
+    }else{
+        console.log("탑승이 불가능합니다.");
+    }
 }
 
-
 // 테스트 코드
-tagNotification(adultTag);
-tagNotification(teenagerTag);
-tagNotification(transferTag);
-tagNotification(errorTag);
-tagNotification(adultTag);
+checkHeight(141);
+checkHeight(135);
+checkHeight(170);
